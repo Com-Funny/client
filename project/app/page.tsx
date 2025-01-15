1c0eb45e57dcf4c45cbdbe987cfa8ac8a5cf183d
jsjang <jsjang@concepters.co.kr>
2025-01-15 15:22:42 +0900

    initialize

--- project/app/page.tsx
+++ project/app/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_API_URL);
+  }, []);
   return <div>배포테스트</div>;
 }
