5eb19ee0fa52e35950576c4330ab37f967e479c4
jsjang <jsjang@concepters.co.kr>
2025-05-19 18:13:49 +0900

    fix: eslint

--- project/eslint.config.mjs
+++ project/eslint.config.mjs
@@ -11,6 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // 사용되지 않은 변수 경고 비활성화
+      "@typescript-eslint/no-unused-vars": "off",
+      // const 권장 오류 비활성화
+      "prefer-const": "off",
+      // any 타입 사용 경고 비활성화
+      "@typescript-eslint/no-explicit-any": "off",
+      // useEffect 종속성 경고 비활성화
+      "react-hooks/exhaustive-deps": "off",
+      // 잘못된 문자 이스케이프 오류 비활성화
+      "react/no-unescaped-entities": "off",
+      // <img> 태그 사용 경고 비활성화
+      "@next/next/no-img-element": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
